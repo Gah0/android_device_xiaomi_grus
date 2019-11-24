ef311ce9bfc54ae5ee5508c3f36523219a5bb892
subdragonzj <zj502407679@gmail.com>
2019-11-24 23:27:34 +0800

    grus: Build wifi packages
    Change-Id: I0cc454e2ca3bea2b07e3ed5f3f55d95a8a17c6c4
    Signed-off-by: Gah0 <g0d3ph@gmail.com>

--- device.mk
+++ device.mk
@@ -1,4 +1,16 @@
 # Wifi
+PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.0-service \
+    hostapd \
+    hostapd_cli \
+    libqsap_sdk \
+    libQWiFiSoftApCfg \
+    libwifi-hal-qcom \
+    libwpa_client \
+    wificond \
+    wpa_supplicant \
+    wpa_supplicant.conf
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
