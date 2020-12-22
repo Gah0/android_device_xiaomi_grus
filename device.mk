f7c5a4b561957417fdd1e532257ba45626fd6e13
Gah0 <g0d3ph@gmail.com>
2020-12-23 02:32:01 +0800

    grus: Build Context hub HAL package
    Signed-off-by: Gah0 <g0d3ph@gmail.com>

--- device.mk
+++ device.mk
@@ -123,6 +123,11 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
+# Context hub HAL
+PRODUCT_PACKAGES += \
+    android.hardware.contexthub@1.0-impl.generic \
+    android.hardware.contexthub@1.0-service
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
